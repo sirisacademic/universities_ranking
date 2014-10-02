3e13489b33edf160847a1ef28b53590605186286
Victor Pascual <victor.pascual@gmail.com>
2014-10-02 12:19:19 +0200

    Updated visualization to show data from 2013

--- app/scripts/directives/parallel.js
+++ app/scripts/directives/parallel.js
@@ -162,19 +162,19 @@ angular.module('arwuApp')
                       .html("<font size='2'>" + d["Rank 2014-15"] + ". " + d["Institution"] + "</font>")
                       .style("visibility", "visible");
 
-                  // retrieve values from last year
-                  var old_values = scope.old_data.filter(function(p) {
-                    return p.Institution == d.Institution;
-                  })
-                  console.log(old_values)
+                  // // retrieve values from last year
+                  // var old_values = scope.old_data.filter(function(p) {
+                  //   return p.Institution == d.Institution;
+                  // })
+                  // console.log(old_values)
 
-                  // show last year's line
-                  svg.selectAll(".compareground")
-                    .data(old_values)
-                    .append("svg:path")
-                      .attr("d", path)
-                      .attr('stroke-dasharray', '2,3')
-                      .style('visibility', 'visible');
+                  // // show last year's line
+                  // svg.selectAll(".compareground")
+                  //   .data(old_values)
+                  //   .append("svg:path")
+                  //     .attr("d", path)
+                  //     .attr('stroke-dasharray', '2,3')
+                  //     .style('visibility', 'visible');
                 
 
                   highlightLine(this);
