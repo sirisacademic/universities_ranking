8499c69e1ac90756b1bb171faad4b9fede35bf64
XavierGimenez <xavi@xavigimenez.net>
2015-01-16 11:12:43 +0100

    fix link bug and ftp task in gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -380,7 +380,7 @@ module.exports = function (grunt) {
           authKey: 'key1'
         },
         src: 'dist/',
-        dest: 'public_html/deploymentlab/rankings/arwu/2014/',
+        dest: 'public_html/lab/rankings/arwu/2014/',
         exclusions: ['dist/**/.DS_Store', 'dist/**/Thumbs.db', 'dist/tmp']
       }
     }
